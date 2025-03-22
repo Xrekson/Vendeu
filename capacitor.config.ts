b2cfc7774bebd029ad94e55abb40dd746edb07cd
Xrekson <indraneel1080brahma@gmail.com>
2025-03-22 11:51:35 +0530

    Native Added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rampage.app',
+  appName: 'rampage',
+  webDir: 'dist'
+};
+
+export default config;
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private registerUrl:string;
-  constructor(private http:HttpClient) {
-    this.registerUrl = environment.baseUrl+ '/user/save';
-   }
+  private registerUrl: string;
+  public userData: BehaviorSubject<{
+    id: string,
+    token: string,
+    username: string
+  }> = new BehaviorSubject({
+    id: "",
+    token: "",
+    username: ""
+  });
 
-  register(data:object): Observable<object>{
-    return this.http.post(this.registerUrl,data);
+  public getUser: Observable<any>;
+
+  constructor(private http: HttpClient) {
+    this.getUser = this.userData.asObservable();
+    this.registerUrl = environment.baseUrl + '/login';
+  }
+
+  register(data: object): Observable<object> {
+    return this.http.post(this.registerUrl, data);
+  }
+  setUser(data: {
+    id: string,
+    token: string,
+    username: string
+  }) {
+    this.userData.next(data);
   }
 }
