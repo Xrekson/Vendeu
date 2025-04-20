1646a69c171c24b33606ebd4e8e50a5373367ee3
Xrekson <indraneel1080brahma@gmail.com>
2025-04-20 20:38:58 +0530

    Capacitor Removed

--- android/app/src/main/java/com/rampage/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.rampage.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.rampage.app',
-  appName: 'rampage',
-  webDir: 'dist'
-};
-
-export default config;
